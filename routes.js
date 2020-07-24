0204a2444d52dbf0615f56d69a428622ea2076c1
Helena Kopp <helena.mkopp@gmail.com>
2020-07-24 16:44:57 -0300

    paginas

---
+++ routes.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const routes = express.Router()
+
+routes.get('/', function(req, res) {
+    return res.render("pages/inicio")
+})
+
+routes.get('/inicio', function(req, res) {
+    return res.render("pages/inicio")
+})
+
+routes.get('/login', function(req, res) {
+    return res.render("pages/login")
+})
+
+routes.get('/cadastro', function(req, res) {
+    return res.render("pages/cadastro")
+})
+
+routes.get('/lista', function(req, res) {
+    return res.render("pages/lista")
+})
+
+routes.get('/nova-tarefa', function(req, res) {
+    return res.render("pages/nova-tarefa")
+})
+
+
+module.exports = routes
